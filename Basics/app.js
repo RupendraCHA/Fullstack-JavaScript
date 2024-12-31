5c0882c379feada1bc6885d077c6e7dc58ebebeb
RupendraCHA <chandaluri210@gmail.com>
2024-12-31 17:47:20 +0530

    Fullstack Javascript

---
+++ Basics/app.js
@@ -0,0 +1,12 @@
+console.log("Rupendra the Developer");
+console.log(2 + 2);
+console.clear();
+console.log({ name: "Rupendra Chandaluri" });
+console.warn("This is a Warning!");
+console.table({ name: "Rupendra", age: 25, village: "Rudravaram" });
+
+let name;
+console.log(name);
+
+let developerName = "Rupendra";
+console.log(developerName);
